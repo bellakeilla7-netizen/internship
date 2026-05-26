d0f9623527069d84aaefcf7bdbe2403d37c70970
brianatugane <brianatugane@gmail.com>
2026-05-26 19:36:29 +0200

    updated navbar

--- bella-intern/src/components/ui/NavBar.vue
+++ bella-intern/src/components/ui/NavBar.vue
@@ -1,11 +1,21 @@
 <template>
-  <nav class="bg-gray-900 text-white p-4">
+  <nav class="bg-green-900 text-white p-4">
     <div class="flex justify-between items-center">
-      
-      
-      <h1 class="text-xl font-bold">~TEMBERA~</h1>
 
-     
+      
+      <div class="flex items-center space-x-2">
+
+        
+        <img
+          src="/logo.jpeg"
+          alt="Logo"
+          class="w-10 h-10 rounded-full"
+        />
+
+        <h1 class="text-xl font-bold">~TEMBERA~</h1>
+      </div>
+
+      
       <div class="space-x-4">
         <a href="#" class="hover:text-blue-400">Home</a>
         <a href="#" class="hover:text-blue-400">About</a>
--- bella-intern/src/views/Home.vue
+++ bella-intern/src/views/Home.vue
@@ -3,6 +3,6 @@
 </script>
 
 <template>
-    <p class="text-center -lg font-bold text-3xl text-blue-300 "> TEMBERA U RWANDA PROJECT</p>
+    <p class="text-center -lg font-bold text-3xl text-green-300 "> TEMBERA U RWANDA PROJECT</p>
 
   </template>
